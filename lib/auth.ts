ca94781f8f1e5d8e87fee680b0b946d610efffdc
vishalprajapati15 <vishalprajapati3266@gmail.com>
2026-03-06 13:39:53 +0530

    next auth done

--- lib/auth.ts
+++ lib/auth.ts
@@ -44,5 +44,28 @@ export const authOptions: NextAuthOptions = {
     })
     // ...add more providers here
   ],
+  callbacks: {
+    async jwt({ token, user, }) {
+      if (user) {
+        token.id = user.id;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      if (session.user) {
+        session.user.id = token.id as string
+      }
+      return session;
+    },
+  },
+  pages: {
+    signIn: "/login",
+    error: "/login",
+  },
+  session: {
+    strategy: "jwt",
+    maxAge: 30 * 24 * 60 * 60
+  },
+  secret:process.env.NEXTAUTH_SECRET
 };
 
