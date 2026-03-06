b81861f1c9f967a6cb44b0993b2fe9572a3a14af
vishalprajapati15 <vishalprajapati3266@gmail.com>
2026-03-06 15:29:05 +0530

    add imagekit

---
+++ app/api/auth/imagekit-auth/route.ts
@@ -0,0 +1,20 @@
+import { getUploadAuthParams } from "@imagekit/next/server"
+
+export async function GET() {
+
+    try {
+        const authenticationParameters = getUploadAuthParams({
+            privateKey: process.env.IMAGEKIT_PRIVATE_KEY as string, // Never expose this on client side
+            publicKey: process.env.NEXT_PUBLIC_PUBLIC_KEY as string,
+        })
+
+        return Response.json({
+            authenticationParameters,
+            publicKey: process.env.NEXT_PUBLIC_PUBLIC_KEY
+        });
+    } catch (error) {
+        return Response.json({
+            error: 'Authentication for ImageKit failed!!'
+        }, { status: 500 });
+    }
+}
